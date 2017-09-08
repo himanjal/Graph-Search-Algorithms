a7e8f58232baed19cc82f87034dc7e2a6431ae1a
himanjal <himanjalsharma1@gmail.com>
2017-09-08 08:28:16 -0700

    add looping

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ from aStarSearch import aStarSearch
 try: import easygui
 except ImportError:pip.main(["install", "easygui"])
 
+#Author: Himanjal Sharma 
+#Run this file to initialize the project 
+
+
+
 class graphNodes:
     node1 = ''
     node2 = ''
@@ -95,8 +100,8 @@ def createWeightedGraph():
 
 def readFile():
 
-    #graph = easygui.fileopenbox(filetypes = "*.txt", multiple= False)
-    graph = "graph2.txt"
+    graph = easygui.fileopenbox(filetypes = "*.txt", multiple= False)
+    #graph = "graph2.txt"
     flag = False
     file = open(graph, "r")
     for line in file:
@@ -132,7 +137,18 @@ def searchSelect(ch):
         aStarSearch(weightGraph, list_of_heuristic, START, GOAL)
         return
 
+def printMenu():
+    menu = "\nEnter the Number of the Search\n"
+    menu = menu + "\t1. Depth 1st search\n"
+    menu = menu + "\t2. Breadth 1st search\n"
+    menu = menu + "\t3. Depth Limited Search\n"
+    menu = menu + "\t4. Uniform Search\n"
+    menu = menu + "\t5. Greedy Search\n"
+    menu = menu + "\t6. A* Search\n"
+    print menu
 
+    choice = int(raw_input("Choose Number:"))
+    return choice
 
 
 
@@ -142,16 +158,16 @@ readFile()
 createGraph()
 createWeightedGraph()
 
-manu = menu + "Name: Himanjal Sharma\n"
-menu = "Enter the Number of the Search\n"
-menu = menu + "\t1. Depth 1st search\n"
-menu = menu + "\t2. Breadth 1st search\n"
-menu = menu + "\t3. Depth Limited Search\n"
-menu = menu + "\t4. Uniform Search\n"
-menu = menu + "\t5. Greedy Search\n"
-menu = menu + "\t6. A* Search\n"
-print menu
 
-choice = int(raw_input("Choose Number:"))
+answer = "y"
 
-searchSelect(choice)
+while (answer.upper() == "Y"):
+    choice = printMenu()
+    searchSelect(choice)
+    answer = raw_input("\nContinue Search with a different method?(Yes = Y, No = N)")
+
+
+
+
+
+
